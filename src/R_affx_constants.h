1e7625c5b3f79a327ab35f2d5639491eb399f4f8
ernursaka <ernursaka@gmail.com>
2017-10-15 17:54:50 -0400

    Add files via upload

---
+++ src/R_affx_constants.h
@@ -0,0 +1,30 @@
+#define R_AFFX_VERBOSE 1
+#define R_AFFX_REALLY_VERBOSE 2
+
+/*
+ * Using R's test of endianness
+ * Files was taken from affxparser R Bioconducter package
+ * which also uses the Affymetrix created fusion_sdk library.
+ * File is required in order to compile fusion_sdk
+ */
+
+#include <Rconfig.h>
+
+#ifdef WORDS_BIGENDIAN
+  #define IS_BIG_ENDIAN 1
+#endif
+
+#ifdef _MSC_VER
+#define _strtoui64 strtoull	/* for TsvFile.cpp */
+#include <w32api.h>
+//#define WINVER WindowsXP	/* for Util.cpp, via TsvFile.cpp */
+#endif
+
+/* Patch for compilation errors ("'int32_t' does not name a type")
+   on Windows using the Rtools 2.15.0.1915-1919 toolchain, which
+   is used by R (> 2.14.1), cf. private email on 'affxparser (devel)
+   on Windows' on Jan 25 - Mar 5, 2012. */
+#ifdef __MINGW32__
+#include <stdint.h>
+#endif
+
